ae56bbc95cc8fa5345ca66cee7f8f6777e126878
Xolani Mgube <xnmgube@gmail.com>
2020-02-07 23:40:19 -0800

    initial commit

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+// document.querySelector('.list-items').style.display = 'none';
+
+
+
+// document.querySelector('.burger').addEventListener('click', function(){
+
+// 		document.querySelector('.list-items').style.display = 'block';
+// })
+
+
+	var hide = document.querySelector('.list-items').style.display = 'none';
+
+function toggling(){
+	document.querySelector('.list-items').style.display = 'block';
+}
+
+var show = document.querySelector('.burger').addEventListener('click', toggling);
+
+
+
+
+
+
+
+
